003e4f86ecd49219d33c8fcde4cd792e4f2c0dbb
oumaoumag <ouma.godwin10@gmail.com>
2025-01-30 10:07:22 +0300

    Fetch comments for each post in the home handler - Added SQL query to fetch comments for each post. - Joined comments with users to get the username. - Ordered comments by creation date. - Hnadled errors for comment fetching

---
+++ internal/handlers/home.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"forum/internal/db"
+	"forum/internal/models"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	query := `
+	SELECT p.post_id, p.title, p.content, u.username, c.name AS category, p.created_at,
+	(SELECT COUNT(*) FROM likes WHERE post_id = p.post_id AND comment_id IS NULL AND like_type = 'like') AS like_count,
+	(SELECT COUNT(*) FROM likes WHERE post_id AND comment_id IS NULL AND like_type = 'dislike') AS dislike_count
+FROM post p
+JOIN users u ON p.user_id = u.user_id
+JOIN categories c ON p.category_id = c.category_id
+ORDER BY p.created_at DESC`
+
+rows, err := db.DB.Query(query)
+if err != nil {
+	log.Println(err)
+	http.Errir(w, "Unable to fetch posts", http.StatusInternalServerError)
+	return
+}
+defer rows.Close()
+
+// // Check if there are any rows
+// If !rows.Next() {
+// http.Error(w, "No posts found", http.StatusNotFOund)
+// return
+// }
+
+var posts []models.Post
+for rows.Next() {
+	var post models.Post
+	err := rows.Scan(&post.PostID, &post.Title, &post.Username, &post.Category, &post.CreatedAt, &post.LikeCount, &post.DislikeCount)
+	if err != nil {
+		http.Error(w, "Error scanning posts", http.StatusInternalServerError)
+		return
+	}
+
+	// Fetch comments for each post
+	commentQuery := `
+	SELECT c.comment_id, c.content, u.username, created_at
+	FROM comments c
+	JOIN users u ON c.user_id = u.user_id
+	WHERE c.post_id = ?
+	ORDER BY c.created_at ASC`
+	commentRows, err := db.DB.Query(commentQuery, post.PostID)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Unable to fetch comments", http.StatusInternalServerError)
+		return
+	}
+
+	// var commments
+}
+
+}
