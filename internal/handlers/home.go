65dd843a7a78f78cebcb63daa9e935efa7e371d9
oumaoumag <ouma.godwin10@gmail.com>
2025-01-30 11:49:15 +0300

    fix typos and spelling errors

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -18,64 +18,64 @@ JOIN users u ON p.user_id = u.user_id
 JOIN categories c ON p.category_id = c.category_id
 ORDER BY p.created_at DESC`
 
-rows, err := db.DB.Query(query)
-if err != nil {
-	log.Println(err)
-	http.Errir(w, "Unable to fetch posts", http.StatusInternalServerError)
-	return
-}
-defer rows.Close()
-
-// // Check if there are any rows
-// If !rows.Next() {
-// http.Error(w, "No posts found", http.StatusNotFOund)
-// return
-// }
-
-var posts []models.Post
-for rows.Next() {
-	var post models.Post
-	err := rows.Scan(&post.PostID, &post.Title, &post.Username, &post.Category, &post.CreatedAt, &post.LikeCount, &post.DislikeCount)
+	rows, err := db.DB.Query(query)
 	if err != nil {
-		http.Error(w, "Error scanning posts", http.StatusInternalServerError)
+		log.Println(err)
+		http.Errir(w, "Unable to fetch posts", http.StatusInternalServerError)
 		return
 	}
+	defer rows.Close()
 
-	// Fetch comments for each post
-	commentQuery := `
+	// // Check if there are any rows
+	// If !rows.Next() {
+	// http.Error(w, "No posts found", http.StatusNotFOund)
+	// return
+	// }
+
+	var posts []models.Post
+	for rows.Next() {
+		var post models.Post
+		err := rows.Scan(&post.PostID, &post.Title, &post.Username, &post.Category, &post.CreatedAt, &post.LikeCount, &post.DislikeCount)
+		if err != nil {
+			http.Error(w, "Error scanning posts", http.StatusInternalServerError)
+			return
+		}
+
+		// Fetch comments for each post
+		commentQuery := `
 	SELECT c.comment_id, c.content, u.username, created_at
 	FROM comments c
 	JOIN users u ON c.user_id = u.user_id
 	WHERE c.post_id = ?
 	ORDER BY c.created_at ASC`
-	commentRows, err := db.DB.Query(commentQuery, post.PostID)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Unable to fetch comments", http.StatusInternalServerError)
-		return
-	}
-
-	var commments []models.Comment
-	for commentRows.Nexr() {
-		err := CommentsRows.Scan(&comment.CommentID, &comment.Username, &comment.CreatedAt)
+		commentRows, err := db.DB.Query(commentQuery, post.PostID)
 		if err != nil {
-			http.Error(w, "Error scanning comments", http.StatusInternalServerError)
+			log.Println(err)
+			http.Error(w, "Unable to fetch comments", http.StatusInternalServerError)
 			return
 		}
-		comments = append(comments, comment)
+
+		var commments []models.Comment
+		for commentRows.Next() {
+			err := CommentsRows.Scan(&comment.CommentID, &comment.Content, &comment.Username, &comment.CreatedAt)
+			if err != nil {
+				http.Error(w, "Error scanning comments", http.StatusInternalServerError)
+				return
+			}
+			comments = append(comments, comment)
+		}
+		commentRows.Close()
+
+		post.Comments = comments
+		posts = append(posts, post)
 	}
-	commentRows.Close()
 
-	post.Comments = comments
-	posts = append(posts, post)
-}
+	tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/home.html"))
 
-tmpl := template.Must(template.ParseFiles("web/templates/layout.html", "web/templates/home.html"))
-
-err = tmpl.Execute(w, posts)
-if err != nil {
-	log.Println(err)
-	http.Error(w, "Unable to render template", http.StatusInternalServerError)
-	return
-}
+	err = tmpl.Execute(w, posts)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Unable to render template", http.StatusInternalServerError)
+		return
+	}
 }
