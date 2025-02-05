fd9f705d4aacdfbb2430a87a97cc028a0e949ddb
sjuma <sheillajuma001@gmail.com>
2025-02-05 11:31:35 +0300

    function to render the error templates

---
+++ internal/utils/error.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"net/http"
+	"text/template"
+	"log"
+)
+
+func DisplayError(w http.ResponseWriter, code int, message string) {
+	data := struct {
+		Code    int
+		Message string
+	}{
+		Code:    code,
+		Message: message,
+	}
+
+	tmpl, err := template.ParseFiles("web/template/error.html")
+	if err != nil {
+		log.Printf("Error loading template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
