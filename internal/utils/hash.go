91eb9e7c11bd39cbe5e04408a6d57a975f0d8078
vmuhembe <muhembelev@gmail.com>
2025-02-06 15:44:10 +0300

    feat: add more special characters

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -15,7 +15,7 @@ func ValidatePassword(password string) bool {
 		hasUpper   = regexp.MustCompile(`[A-Z]`).MatchString
 		hasLower   = regexp.MustCompile(`[a-z]`).MatchString
 		hasNumber  = regexp.MustCompile(`[0-9]`).MatchString
-		hasSpecial = regexp.MustCompile(`[!@#~$%^&*()_+|<>?:{}]`).MatchString
+		hasSpecial = regexp.MustCompile(`[!\"#$%&'()*+,\-./:;<=>?@[\\\]^_{|}~]`).MatchString
 	)
 
 	return hasUpper(password) && hasLower(password) && hasNumber(password) && hasSpecial(password)
