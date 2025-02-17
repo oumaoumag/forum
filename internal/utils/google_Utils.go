2f2d3879d40b85763cd6b1bfea91c458bda36de5
oumaoumag <ouma.godwin10@gmail.com>
2025-02-17 12:13:05 +0300

    feat : Add FindOrCreateGooleUser 1. Checks for existing user by google id 2. Fallback check by email 3. Creates a new user 4.

--- internal/utils/google_Utils.go
+++ internal/utils/google_Utils.go
@@ -3,8 +3,11 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"forum/internal/db"
 	"forum/internal/models"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 var httpClient = &http.Client{}
@@ -28,4 +31,50 @@ func GetGoogleUserInfo(accessToken string) (*models.GoogleUser, error) {
 		return nil, fmt.Errorf("failed to decode user info: %w", err)
 	}
 	return &user, nil
+}
+
+// FindOrCreateGoogleUser() finds a new user in the database based on the information provided by Google's OAuth2 API.
+func FindOrCreateGoogleUser(googleUser *models.GoogleUser) (int, error) {
+	var userID int
+
+	// check if user exists by Google ID
+	err := db.DB.QueryRow("SELECT user_id FROM users WHERE provider_id = ? AND auth_type = 'google'", googleUser.ID).Scan(&userID)
+	if err == nil {
+		return userID, nil
+	}
+
+	// Fallback check by email
+	err = db.DB.QueryRow("SELECT user_id FROM users WHERE email = ?", googleUser.Email).Scan(&userID)
+	if err == nil {
+		// Update existing user with Goole info
+		_, err = db.DB.Exec(
+			"UPDATE users SET provider_id = ?, auth_type = 'google' WHERE user_id = ?",
+			googleUser.ID,
+			userID,
+		)
+		return userID, err
+	}
+
+	// Create new user
+	username := googleUser.Name
+	if username == "" {
+		username = "google_user_" + uuid.New().String()[:8]
+	}
+
+	result, err := db.DB.Exec(
+		"INSERT INTO users (email, username, password, auth_type, provider_id) VALUES (?,?,? 'google', ?)",
+		googleUser.Email,
+		username,
+		"oauth_placeholder", // Password placeholder
+		googleUser.ID,
+	)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("failed to create user: %w", err)
+	}
+	return int(id), nil
 }
