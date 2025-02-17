118b4a846ccd07dcb1549630d4ee9ad3ebc4ea76
oumaoumag <ouma.godwin10@gmail.com>
2025-02-17 11:42:02 +0300

    fix typos in variable naming

--- internal/models/googleUser.go
+++ internal/models/googleUser.go
@@ -5,7 +5,7 @@ package models
 type GoogleUser struct {
 	ID           string `json:"id"`
 	Email        string `json:"email"`
-	VeriiedEmail bool   `json:"verified_email"`
+	VerifiedEmail bool   `json:"verified_email"`
 	Name         string `json:"name"`
 	GivenName    string `json:"given_name"`
 	FamilyName   string `json:"family_name"`
