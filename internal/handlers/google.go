67314e751979272189f05c4ba12db0ee9bf5ff91
oumaoumag <ouma.godwin10@gmail.com>
2025-02-17 09:40:29 +0300

    feat: Add init func to 1. Load Environment Variables 2. Retrieve Environment Variables 3. Check Environment Variables 4. Configer OAuth2

--- internal/handlers/google.go
+++ internal/handlers/google.go
@@ -1,9 +1,42 @@
 package handlers
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 var (
 	googleOuthConfig *oauth2.Config
 )
+
+
+func init() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
+	googleClientID := os.Getenv("GOOGLE_CLIENT_ID")
+	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	redirectURL := os.Getenv("GOOGLE_REDIRECT_URL")
+
+	if googleClientID == "" || googleClientSecret == "" || redirectURL == "" {
+		log.Printf("Warning: Google 0Auth environment  variables not fully configured")
+		return
+	}
+
+	googleOuthConfig = &oauth2.Config{
+		ClientID:     googleClientID,
+		ClientSecret: googleClientSecret,
+		RedirectURL:  redirectURL,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"https://googleapis.com/auth/userinfo.email",
+		},
+		Endpoint: google.Endpoint,
+	}
+}
