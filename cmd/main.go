6f572a81b1e6ff96fc49abcc3e25a8f694d7a953
garveyshah <garveyshah1595@gmail.com>
2025-04-21 21:11:30 +0300

    refactor: remove unused HTTPS redirection and TLS configuration code

--- cmd/main.go
+++ cmd/main.go
@@ -4,30 +4,19 @@ import (
 	"log"
 	"net/http"
 	"time"
-	"crypto/tls"
-	"golang.org/x/crypto/acme/autocert"
+
+	// "golang.org/x/crypto/acme/autocert"
 
 	"forum/internal/auth"
 	"forum/internal/db"
 	"forum/internal/handlers"
+	"forum/internal/server"
 )
-
-// handles HTTP to HTTPS redirection:
-func redirectToHTTPS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("X-Forwarded-Proto") != "https" {
-			sslUrl := "https://" + r.Host + r.RequestURI
-			http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	// Initialize the database
 	if err := db.Init("./forum.db"); err != nil {
-		log.Fatal("Database initialization failed: ", err)
+		log.Fatal("Database initialization failed: ", e\rr)
 	}
 
 	go db.ScheduleSessionCleanup(1*time.Hour, db.CleanupExpiredSessions)
@@ -48,22 +37,6 @@ func main() {
 	mux.Handle("/like", auth.SessionMiddleware(auth.RequireAuth(http.HandlerFunc(handlers.LikeHandler))))
 	mux.Handle("/logout", auth.SessionMiddleware(auth.RequireAuth(http.HandlerFunc(handlers.LogoutHandler))))
 
-	// TLS Configuration
-	tlsConfig := &tls.Config{
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-		},
-		PreferServerCipherSuites: true,
-	}
-
-	// 	HTTPS Server
-	httpsServer := &http.Server{
-		Addr: ":443"
-	}
 	// Register GitHub OAuth routes with the same mux
 	mux.HandleFunc("/auth/github", handlers.GitHubLoginHandler)
 	mux.HandleFunc("/oauth2/callback/github", handlers.GitHubCallbackHandler)
@@ -72,13 +45,14 @@ func main() {
 	mux.HandleFunc("/auth/google", handlers.GoogleLoginHandler)
     mux.HandleFunc("/auth/callback/google", handlers.GoogleCallbackHandler)
 
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
+	// server configuration
+	config := server.NewDefaultConfig(mux)
 
-	log.Println("Server started at http://localhost:8080")
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	// Initialize & start server
+	srv := server.New(config)
+	if err := srv.Start(); err != nil {
+		log.Fatal("Server error:", err)
+	} 
+
+	
 }
