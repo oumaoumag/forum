c1ac4c8ad7a09f76ba7f463aa10f4762c3e0f255
garveyshah <garveyshah1595@gmail.com>
2025-04-21 20:21:50 +0300

    feat: implement HTTP to HTTPS redirection and enhance database error logging

--- cmd/main.go
+++ cmd/main.go
@@ -4,22 +4,38 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"crypto/tls"
+	"golang.org/x/crypto/acme/autocert"
 
 	"forum/internal/auth"
 	"forum/internal/db"
 	"forum/internal/handlers"
 )
 
+// handles HTTP to HTTPS redirection:
+func redirectToHTTPS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-Forwarded-Proto") != "https" {
+			sslUrl := "https://" + r.Host + r.RequestURI
+			http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// Initialize the database
 	if err := db.Init("./forum.db"); err != nil {
-		log.Println("error:", err)
+		log.Fatal("Database initialization failed: ", err)
 	}
 
 	go db.ScheduleSessionCleanup(1*time.Hour, db.CleanupExpiredSessions)
 
+	// Main router creation
 	mux := http.NewServeMux()
 
+	// Static file handling
 	fs := http.FileServer(http.Dir("web/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
@@ -32,6 +48,22 @@ func main() {
 	mux.Handle("/like", auth.SessionMiddleware(auth.RequireAuth(http.HandlerFunc(handlers.LikeHandler))))
 	mux.Handle("/logout", auth.SessionMiddleware(auth.RequireAuth(http.HandlerFunc(handlers.LogoutHandler))))
 
+	// TLS Configuration
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
+		PreferServerCipherSuites: true,
+	}
+
+	// 	HTTPS Server
+	httpsServer := &http.Server{
+		Addr: ":443"
+	}
 	// Register GitHub OAuth routes with the same mux
 	mux.HandleFunc("/auth/github", handlers.GitHubLoginHandler)
 	mux.HandleFunc("/oauth2/callback/github", handlers.GitHubCallbackHandler)
